23155998369fe82f8d8e7a2b4deb069347533ab2
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-08-17 11:08:47 -0300

    feat: ex04 toggle visibility of div.app
    The functionality was done as described, using the app class.
    This can cause side effects due to the volatility of using classes as a selector.
    This will change the visibility of all elements that use the app class.

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
+const visibilityLink = document.querySelector('[data-js="visibility-link"]')
+const appCSSClass = document.querySelector('.app')
+
 document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+visibilityLink.addEventListener('click', (event) => {
+  event.preventDefault()
+  appCSSClass.classList.toggle('not-visible')
+}, false)
