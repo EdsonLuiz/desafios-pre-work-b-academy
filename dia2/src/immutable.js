5ace10a53e4647b2ae1b091254e45cad06471935
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-08-18 09:52:11 -0300

    refactor: d2_ex01 create immutable objects
    To solve deep copy of nested object was used JSON methods.
    At the end was added a new console log for an extra check.

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -5,10 +5,12 @@ const john = {
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
+const jane = JSON.parse(JSON.stringify(john))
+
 
 jane.name = 'Jane'
 jane.hobbies.push('MuayThai', 'Programming')
 
 console.log('John:', john)
 console.log('Jane:', jane)
+console.log(`Objects are the same? ` + Object.is(jane, john))
