8bbeaa979c5d5940457030665c40fefe26d81b3e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-08-20 11:22:23 -0300

    feat: d3_ex02 add select multiple to form
    Fieldset for color select was created to maintain consistency with previous field.
    Some CSS are added to support and simplify JS code.
    Some functions was created to increase readability of array methods and avoid duplicated code.

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,11 @@
 const inputName = document.querySelector('[data-js="username"]')
+const form = document.querySelector('[data-js="simple"]')
+
+const select = document.createElement('select')
+const fieldset = document.createElement('fieldset')
+const legend = document.createElement('legend')
+
+const cores = ['red', 'green', 'tomato', 'blue', 'pink']
 
 function withUpperCaseMatch(match) {
   return match.toUpperCase()
@@ -19,3 +26,46 @@ function handleInputChange(e) {
 }
 
 inputName.addEventListener('input', handleInputChange)
+
+// EX 02
+// add fieldset to form
+legend.textContent = 'Cores'
+fieldset.appendChild(legend)
+form.appendChild(fieldset)
+
+// add select to fieldset and add options
+fieldset.appendChild(select)
+select.setAttribute('multiple', '')
+
+
+const divColorsContainer = document.createElement('div')
+divColorsContainer.className = 'color-container'
+
+cores.forEach(cor => {
+  const option = document.createElement('option')
+  option.value = cor
+  option.textContent = cor
+  select.appendChild(option)
+})
+
+function toNewColorDiv(option) {
+  const div = document.createElement('div')
+  div.className = 'color-div'
+  div.style.background = option.value
+  return div
+}
+
+function elementAppendToContainer(element) {
+  divColorsContainer.appendChild(element)
+}
+
+function handleSelectChangeEvent(e) {
+  divColorsContainer.innerHTML = ''
+  const arrayOptions = [...e.target.selectedOptions]
+  arrayOptions
+    .map(toNewColorDiv)
+    .forEach(elementAppendToContainer)
+  fieldset.appendChild(divColorsContainer)
+}
+
+select.addEventListener('change', handleSelectChangeEvent )
